0ea4465e9d776f0a4605b5e2f2b99fd54b8a97b5
Roberto Freire <roberto.freire@pasajerosquito.gob.ec>
2025-06-04 13:32:57 -0500

    project base, added git ignore, added basic dependencies

---
+++ src/test/java/com/dentalsoft/finantialManagement/FinantialManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentalsoft.finantialManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinantialManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
